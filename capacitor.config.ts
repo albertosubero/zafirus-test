9ea69c1d16f3afaa8a512c5edd649039f57b0122
avsg1991@gmail.com <avsg1991@gmail.com>
2024-07-12 16:04:18 -0400

    setup project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ZafirusTest',
+  webDir: 'www'
+};
+
+export default config;
